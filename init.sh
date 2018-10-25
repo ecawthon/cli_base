ea9e45c00032a4c978793cfddae8c79cb481637a
Eleanor Cawthon <cawthon.e@gmail.com>
2018-10-24 17:05:31 -0700

    vim: Fix git submodule

--- init.sh
+++ init.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-ln -s $HOME/cli_base/.* $HOME/ && ls -la ~
+ln -s $PWD/.* $HOME/ && ls -la ~
 cd ~
 ./.zprezto/init.sh
 cd ~/.vim
-git submodule --init
+git submodule init
+git submodule update
 vim +PluginInstall +qall
