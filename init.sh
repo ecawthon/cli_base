2f46f013dc69ebaeb7c065b9f153e60ce7e0d323
Eleanor Cawthon <cawthon.e@gmail.com>
2018-10-24 23:00:16 -0700

    clean up submodules

--- init.sh
+++ init.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+sudo apt-get install zsh tmux neovim ctags
+git submodule init
+git submodule update
 ln -s $PWD/.* $HOME/ && ls -la ~
-cd ~
-./.zprezto/init.sh
+ln -s ~/.vim/vimrc ~/.vim/init.vim
 cd ~/.vim
 git submodule init
 git submodule update
 vim +PluginInstall +qall
+setopt EXTENDED_GLOB
+/bin/zsh zinit.sh
