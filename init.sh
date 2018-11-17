12e527e722eb4cbffdc959cfb031aa052a0f8dcb
Eleanor Cawthon <cawthon.e@gmail.com>
2018-11-17 14:49:55 -0800

    fix linking

--- init.sh
+++ init.sh
@@ -2,7 +2,7 @@
 sudo apt-get install zsh tmux neovim ctags ranger
 git submodule update --init --recursive
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-ln -s $DIR/tmux.conf $HOME/.vim
+ln -s $DIR/tmux.conf $HOME/.tmux.conf
 ln -s $DIR/vim $HOME/.vim
 mkdir -p $HOME/.config/nvim
 ln -s $DIR/vim/init.vim $HOME/.config/nvim/init.vim
