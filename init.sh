759433507ccaa3b88130346c9fe3c24f30864dc3
Eleanor Cawthon <cawthon.e@gmail.com>
2018-11-17 12:04:27 -0800

    fix nvim

--- init.sh
+++ init.sh
@@ -4,7 +4,8 @@ git submodule update --init --recursive
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ln -s $DIR/tmux.conf $HOME/.vim
 ln -s $DIR/vim $HOME/.vim
-ln -s $DIR/vim/vimrc $HOME/.vim/init.vim
+mkdir -p $HOME/.config/nvim
+ln -s $DIR/vim/init.vim $HOME/.config/nvim/init.vim
 cd $HOME/.vim
 git submodule update --init
 vim +PluginInstall +qall
