cc0448444415b39e3e6a0c4eac372ed4e7d6ae2f
Eleanor Cawthon <cawthon.e@gmail.com>
2019-09-18 20:42:46 -0700

    fix init scripts

--- zinit.zsh
+++ zinit.zsh
@@ -1,6 +1,9 @@
 #!/bin/zsh
 setopt EXTENDED_GLOB
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+ZDOTDIR=$DIR/zdotfiles
 for rcfile in "${ZDOTDIR:-$PWD}"/^README.md(.N); do
     (ln -s "$rcfile" "$HOME/.${rcfile:t}" || true)
 done
+ln -s $DIR/zprezto $HOME/.zprezto
 sudo chsh -s /bin/zsh
