e0e117e2565a8ad36cfcf9198a8322fcb973d3ab
Eleanor Cawthon <cawthon.e@gmail.com>
2022-10-01 14:14:17 -0700

    use sudo for prompt copy

--- zinit.zsh
+++ zinit.zsh
@@ -6,5 +6,5 @@ for rcfile in "${ZDOTDIR:-$PWD}"/^README.md(.N); do
     (ln -s "$rcfile" "$HOME/.${rcfile:t}" || true)
 done
 ln -s $DIR/zprezto $HOME/.zprezto
-cp $ZDOTDIR/prompt_adam3_setup /usr/share/zsh/functions/Prompts
+sudo cp $ZDOTDIR/prompt_adam3_setup /usr/share/zsh/functions/Prompts
 sudo chsh -s /bin/zsh
