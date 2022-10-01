60e2cbe472fe9640cb2501aea4e8c4f7f339ba52
ecawthon <cawthon.e@gmail.com>
2022-10-01 13:31:51 -0700

    ctags package name has changed in 22.04

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 sudo apt-get update
-sudo apt-get install zsh tmux neovim ctags ranger
+sudo apt-get install zsh tmux neovim exuberant-ctags ranger
 git submodule update --init --recursive
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ln -s $DIR/tmux.conf $HOME/.tmux.conf
