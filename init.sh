c76694a7e53797da773374da20fa5696dcad53b9
Eleanor Cawthon <cawthon.e@gmail.com>
2022-10-01 14:13:45 -0700

    ignore file exists

--- init.sh
+++ init.sh
@@ -3,7 +3,7 @@ sudo apt-get update
 sudo apt-get install zsh tmux neovim exuberant-ctags ranger
 git submodule update --init --recursive
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-ln -s $DIR/tmux.conf $HOME/.tmux.conf
-ln -s $DIR/ranger $HOME/.config/ranger
+ln -s $DIR/tmux.conf $HOME/.tmux.conf && true
+ln -s $DIR/ranger $HOME/.config/ranger && true
 ./vim_init.sh
 ZDOTDIR=$DIR/zdotfiles /bin/zsh $DIR/zinit.zsh
